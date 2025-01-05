b3cce118b07f4153c9a09a6c3c4f37e3c66f4630
Vikram Singh <nagwalanshu@gmail.com>
2025-01-05 13:36:28 +0530

    chores: fixing smtp bugs

--- server/src/test/email-test.ts
+++ server/src/test/email-test.ts
@@ -1,28 +1,12 @@
-import { Resend } from "resend";
 import { generateOTP } from "../service/user-validation";
+import { sendMailtoUser } from "../utils/emailSender";
 
-const secret = Bun.env.RESEND_API_KEY;
 
-const resend = new Resend(secret);
-
-async function sendMail(to: string, subject: string) {
-	const otp = await generateOTP();
-	console.log("otp: ", otp);
-
-	const { data, error } = await resend.emails.send({
-		from: "Acme <onboarding@resend.dev>",
-		to: [to],
-		subject: String(subject),
-		// react: React.createElement(EmailTemplate, { otp }),
-		text: `your otp is ${otp}`,
-	});
-
-	if (error) {
-		console.log(error);
-		throw new Error("Got error while sending email, resend later");
-	}
-	// console.log(data)
-	return data;
+async function sendMail(email: string) {
+	const otp = await generateOTP()
+	console.log(otp)
+	const mailInfo = await sendMailtoUser(email, otp)
+	console.log(mailInfo)
 }
 
-sendMail("vikramnagwal@gmail.com", "testing email sending");
+sendMail('vikramnagwal@gmail.com')
---
+++ server/src/utils/emailSender.ts
@@ -0,0 +1,32 @@
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+    host: Bun.env.SMTP_HOST,
+    port: Number(Bun.env.SMTP_PORT),
+    secure: Number(Bun.env.SMTP_PORT) === 465, // true for port 465, false for other ports
+    auth: {
+        user: Bun.env.SMTP_USER,
+        pass: Bun.env.SMTP_PASS
+    }
+})
+
+const otpTemplate = (otp: string) => `<b>Please verify your email address</b><br><br><h1>${otp}</h1>`
+
+async function sendMailtoUser(email: string, otp: string) {
+try {
+        const sentMail = await transporter.sendMail({
+            from: `"OTP Verification" <${Bun.env.SMTP_USER}>`,
+            to: String(email),
+            subject: 'Email Verification',
+            text: 'Please verify your email address',
+            html: otpTemplate(otp)
+        })
+
+        return sentMail
+} catch (error) {
+    console.log(error)
+    throw new Error('Unable to send email. Email sending utility malfunctioned! please check the logs')
+}
+}
+
+export { sendMailtoUser }

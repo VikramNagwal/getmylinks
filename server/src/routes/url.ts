034b6562e082830310b64682bd430c3ea91ca035
Vikram Singh <nagwalanshu@gmail.com>
2025-01-08 14:41:49 +0530

    feat: created url service for methods

--- server/src/routes/url.ts
+++ server/src/routes/url.ts
@@ -7,13 +7,20 @@ const urlRouter = new Hono();
 urlRouter.get("/shorten", async (c: Context) => {
 	const body = await c.req.parseBody();
 	const { url, custom } = body;
-	if (!url) return c.json( ApiError.notFound('Url is required!'));
+	if (!url) return c.json(ApiError.notFound("Url is required!"));
 
 	try {
-		// const shortUrl = await 
+		// const shortUrl = await
 	} catch (error) {
-		logger.error('Unable to short url! Please try again later', error);
-		c.json( new ApiError('Unable to short url! Please try again later', 500, true, error));
+		logger.error("Unable to short url! Please try again later", error);
+		c.json(
+			new ApiError(
+				"Unable to short url! Please try again later",
+				500,
+				true,
+				error,
+			),
+		);
 	}
 });
 

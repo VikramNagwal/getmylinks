de256e5e79cc4519dbf97b252c776876ff485ce8
Vikram Singh <nagwalanshu@gmail.com>
2025-01-09 22:28:53 +0530

    dev: link analytics implementation

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,6 +12,7 @@ import { appRouter } from "./routes";
 import { HttpStatusCode } from "./types/types";
 import { ShortUrlSchema } from "./schemas/link-schema";
 import db from "./config/db";
+import { UAParser } from "ua-parser-js";
 
 // middlewares
 app.use(logger());
@@ -23,41 +24,52 @@ app.route("/api/v1/", appRouter);
 app.route("/admin/queues", dashboardApp);
 app.get("/", (c) => c.text("hey from the server!"));
 app.get("/:shorturl", async (c: Context) => {
-	try {
-		const shortUrl = ShortUrlSchema.parse(c.req.param("shorturl"));
+  try {
+    const shortUrl = ShortUrlSchema.parse(c.req.param("shorturl"));
+    const userAgent = c.req.header('User-Agent')
+    const referer = c.req.header('referer')
+    const ipAddress = c.req.header('x-forwarded-for') || c.req.header('x-real-ip')
 
-		const response = await db.urlMapping.findUnique({
-			where: {
-				shortUrl,
-			},
-			select: {
-				longUrl: true,
-				isActive: true,
-				expiresAt: true,
-			},
-		});
+    const parser = new UAParser(userAgent);
+    const userAgentInfo = parser.getResult();
 
-		if (!response) {
-			return c.json(
-				{
-					success: false,
-					message: "short url not found! Please check again",
-				},
-				HttpStatusCode.NotFound,
-			);
-		}
-		return c.redirect(response?.longUrl);
-	} catch (error) {
-		return c.json(
-			{
-				success: false,
-				isOperationl: true,
-				message: "Error while redirecting to short url",
-				error,
-			},
-			HttpStatusCode.InternalServerError,
-		);
-	}
+    const response = await db.urlMapping.findUnique({
+      where: { shortUrl },
+      select: {
+        longUrl: true,
+        isActive: true,
+        expiresAt: true,
+      },
+    });
+
+    if (!response) {
+      return c.json({
+        success: false,
+        message: "short url not found! Please check again",
+      }, HttpStatusCode.NotFound);
+    }
+
+    await db.clickEvent.create({
+      data: {
+        shortUrl,
+        userAgent: userAgent || null,
+        referer: referer || null,
+        ipAddress: ipAddress || null,
+        browser: userAgentInfo.browser.name || null,
+        os: userAgentInfo.os.name || null,
+        views: 1
+      },
+    });
+
+    return c.redirect(response.longUrl);
+  } catch (error) {
+    return c.json({
+      success: false,
+      isOperationl: true,
+      message: "Error while redirecting to short url",
+      error,
+    }, HttpStatusCode.InternalServerError);
+  }
 });
 
 export default app;

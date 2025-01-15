dd5b4b9af37400622e8410565d3c5bc04374e17b
Vikram Singh <nagwalanshu@gmail.com>
2025-01-15 22:20:50 +0530

    client: adding profile setup

--- client/src/schemas/profile-schema.ts
+++ client/src/schemas/profile-schema.ts
@@ -1,5 +1,9 @@
-import { z } from "zod"
+import { z } from "zod";
 
 export const profileSchema = z.object({
-    bio: z.string().min(3, "don't know enough about yourself??").max(999, "calm down! 🙂").optional(),
-})
+	bio: z
+		.string()
+		.min(3, "don't know enough about yourself??")
+		.max(999, "calm down! 🙂")
+		.optional(),
+});

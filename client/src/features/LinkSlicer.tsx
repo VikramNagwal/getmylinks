17618866e534db8f3204f10f854897bfa197131c
Vikram Singh <nagwalanshu@gmail.com>
2025-01-23 21:36:29 +0530

    client: added required ui packs from ui library

--- client/src/features/LinkSlicer.tsx
+++ client/src/features/LinkSlicer.tsx
@@ -1,32 +1,31 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-
 interface LinkState {
-    links: string[];
-    isActive: boolean;
-    expiresAt: Date | null;
+	links: string[];
+	isActive: boolean;
+	expiresAt: Date | null;
 }
 
 const initialState: LinkState = {
-    links: [],
-    isActive: true,
-    expiresAt: null
-}
+	links: [],
+	isActive: true,
+	expiresAt: null,
+};
 
 export const LinkSlicer = createSlice({
-    name: "links",
-    initialState,
-    reducers: {
-        setLinks(state, action: PayloadAction<string[]>) {
-            state.links = action.payload;
-        },
-        setIsActive(state, action: PayloadAction<boolean>) {
-            state.isActive = action.payload;
-        },
-        setExpiresAt(state, action: PayloadAction<Date>) {
-            state.expiresAt = action.payload;
-        }
-    }
-})
+	name: "links",
+	initialState,
+	reducers: {
+		setLinks(state, action: PayloadAction<string[]>) {
+			state.links = action.payload;
+		},
+		setIsActive(state, action: PayloadAction<boolean>) {
+			state.isActive = action.payload;
+		},
+		setExpiresAt(state, action: PayloadAction<Date>) {
+			state.expiresAt = action.payload;
+		},
+	},
+});
 
 export const { setLinks, setIsActive, setExpiresAt } = LinkSlicer.actions;
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -9,7 +9,6 @@ const Dashboard = () => {
 				<Navbar />
 			</nav>
 			<main className="p-2 mt-[60px]">
-				
 				<section className="flex flex-col items-center justify-center space-y-4">
 					<h1 className="font-heading">Paste or write your link below</h1>
 					<ShortnForm />

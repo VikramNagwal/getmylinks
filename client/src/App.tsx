286e91a0322c2985d03d1f98a545f502672c9e0c
Vikram Singh <nagwalanshu@gmail.com>
2025-01-24 21:19:38 +0530

    [client]: added features to verification page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,6 @@ import PhonePreview from "./components/Phone-preview.tsx";
 import Dashboard from "./pages/Dashboard.tsx";
 import VerificationPage from "./pages/VerificationPage.tsx";
 
-
 const user = {
 	avatar:
 		"https://images.unsplash.com/photo-1736924862365-9038a7e1be81?q=80&w=1899&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -3,27 +3,27 @@ import ShortnForm from "@/components/forms/ShortnForm";
 import { ModeToggle } from "@/components/Mode-toggle";
 import { Separator } from "@/components/ui/separator";
 import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
+	SidebarInset,
+	SidebarProvider,
+	SidebarTrigger,
 } from "@/components/ui/sidebar";
 
 const Dashboard = () => {
-  return (
-    <SidebarProvider className="font-roboto">
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-          <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2 h-4" />
-          <ModeToggle />
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4">
-          <ShortnForm />
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
-  );
-}
+	return (
+		<SidebarProvider className="font-roboto">
+			<AppSidebar />
+			<SidebarInset>
+				<header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+					<SidebarTrigger className="-ml-1" />
+					<Separator orientation="vertical" className="mr-2 h-4" />
+					<ModeToggle />
+				</header>
+				<div className="flex flex-1 flex-col gap-4 p-4">
+					<ShortnForm />
+				</div>
+			</SidebarInset>
+		</SidebarProvider>
+	);
+};
 
 export default Dashboard;

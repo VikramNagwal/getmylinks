da74b1f06c0009d475444febc2c4f845b2db7837
Vikram Singh <nagwalanshu@gmail.com>
2025-01-26 22:22:12 +0530

    dev: fixed minor bugs

--- client/src/pages/VerificationPage.tsx
+++ client/src/pages/VerificationPage.tsx
@@ -2,23 +2,23 @@ import { VerifyForm } from "@/components/forms/VerifyForm";
 
 const VerificationPage = () => {
 	return (
-		<div className="overflow-hidden">
-			<div className="flex h-[700px]">
-				{/* Left half */}
-				<div className="flex-1 p-8 bg-[#FFDE59]">
-					<VerifyForm />
-				</div>
-				{/* Right half */}
-				<div className="flex-1 hidden md:flex items-center justify-center">
-					<img
-						src="../images/design.webp"
-						alt="doodles"
-						className="w-full h-full object-cover"
-					/>
-				</div>
-			</div>
-		</div>
-	);
+    <div className="overflow-hidden">
+      <div className="flex h-[700px]">
+        {/* Left half */}
+        <div className="flex-1 p-8 bg-[#FFDE59]">
+          <VerifyForm />
+        </div>
+        {/* Right half */}
+        <div className="flex-1 hidden md:flex items-center justify-center">
+          <img
+            src="../images/design.webp"
+            alt="doodles"
+            className="w-full h-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default VerificationPage;

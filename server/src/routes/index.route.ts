419896049895187156be7c96cb3be2fef3aea65c
Vikram Singh <nagwalanshu@gmail.com>
2025-02-19 22:23:56 +0530

    feat: add account management routes and restructure user route for better organization

--- server/src/routes/index.route.ts
+++ server/src/routes/index.route.ts
@@ -1,14 +1,14 @@
 import { Hono } from "hono";
 import { authRouter } from "./auth.route";
 import { urlRouter } from "./links.route";
-import { userRouter } from "./user.route";
+import { userRouter } from "./account.route";
 import { checkRouter } from "./check.route";
 
 const appRouter = new Hono();
 
 appRouter.route("/auth", authRouter);
 appRouter.route("/url", urlRouter);
-appRouter.route("/user", userRouter);
+appRouter.route("/account", userRouter);
 appRouter.route("/check", checkRouter);
 
 export { appRouter };
